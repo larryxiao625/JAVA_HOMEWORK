fd2ce9fb91c54054d36be716994589845caa7748
ray <romerxzh@gmail.com>
2019-10-19 00:55:37 +0800

    Finish the Firse Homework

---
+++ src/bean/UsersBean.java
@@ -0,0 +1,30 @@
+package bean;
+
+public class UsersBean extends Object{
+    String userName;
+    String pass;
+
+    public UsersBean() {
+    }
+
+    public UsersBean(String userName, String pass) {
+        this.userName = userName;
+        this.pass = pass;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+}
