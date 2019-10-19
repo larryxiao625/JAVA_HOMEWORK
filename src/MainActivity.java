b4d36b0fc57f85882c5ca68da35822e757b0bc53
ray <romerxzh@gmail.com>
2019-10-19 17:39:31 +0800

    Add Create Database step

--- src/MainActivity.java
+++ src/MainActivity.java
@@ -14,13 +14,24 @@ public class MainActivity {
 
     public static void main(String[] args) {
         connection = SqlUtil.getConnection();
+        createDatabase();
         createTable();
         insertData();
         insertDataStep3();
         deleteData();
     }
 
-
+    /**
+     * 第0步：新建数据库操作
+     */
+    private static void createDatabase() {
+        String createDatabase = "CREATE DATABASE test;";
+        try {
+            SqlUtil.progressSql(createDatabase, 1);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     /**
      * 第一步建表操作
@@ -52,15 +63,15 @@ public class MainActivity {
      * 第二部插入数据操作
      */
     private static void insertData() {
-        List<Object> usersList=new ArrayList<>();
-        List<Object> personList=new ArrayList<>();
-        usersList.add(new UsersBean("ly","123456"));
-        usersList.add(new UsersBean("liming","345678"));
-        usersList.add(new UsersBean("test","111111"));
-        usersList.add(new UsersBean("test1","123456"));
-        personList.add(new PersonBean("ly","雷力",-1,null));
-        personList.add(new PersonBean("liming","李明",25,null));
-        personList.add(new PersonBean("test","测试用户",20,"13388449933"));
+        List<Object> usersList = new ArrayList<>();
+        List<Object> personList = new ArrayList<>();
+        usersList.add(new UsersBean("ly", "123456"));
+        usersList.add(new UsersBean("liming", "345678"));
+        usersList.add(new UsersBean("test", "111111"));
+        usersList.add(new UsersBean("test1", "123456"));
+        personList.add(new PersonBean("ly", "雷力", -1, null));
+        personList.add(new PersonBean("liming", "李明", 25, null));
+        personList.add(new PersonBean("test", "测试用户", 20, "13388449933"));
         SqlUtil.insertData(usersList);
         SqlUtil.insertData(personList);
         try {
@@ -76,13 +87,13 @@ public class MainActivity {
     /**
      * 第三步插入数据
      */
-    private static void insertDataStep3(){
-        List<Object> personList=new ArrayList<>();
-        personList.add(new PersonBean("ly","王五",-1,null));
-        personList.add(new PersonBean("test2","测试用户2",2,null));
-        personList.add(new PersonBean("test1","测试用户1",33,null));
-        personList.add(new PersonBean("test","张三",23,"18877009966"));
-        personList.add(new PersonBean("admin","admin",-1,null));
+    private static void insertDataStep3() {
+        List<Object> personList = new ArrayList<>();
+        personList.add(new PersonBean("ly", "王五", -1, null));
+        personList.add(new PersonBean("test2", "测试用户2", 2, null));
+        personList.add(new PersonBean("test1", "测试用户1", 33, null));
+        personList.add(new PersonBean("test", "张三", 23, "18877009966"));
+        personList.add(new PersonBean("admin", "admin", -1, null));
         SqlUtil.insertData(personList);
         try {
             SqlUtil.showTable(Objects.requireNonNull(SqlUtil.progressSql(showUsersTable, 1)));
@@ -95,8 +106,8 @@ public class MainActivity {
     /**
      * 第四步删除数据
      */
-    private static void deleteData(){
-        SqlUtil.deleteUser("test",2);
+    private static void deleteData() {
+        SqlUtil.deleteUser("test", 2);
         try {
             SqlUtil.showTable(Objects.requireNonNull(SqlUtil.progressSql(showUsersTable, 1)));
             SqlUtil.showTable(Objects.requireNonNull(SqlUtil.progressSql(showPersonTable, 1)));
